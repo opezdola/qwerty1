439a18442b2d754ba821d913d7d228ae76c397b7
Максим Некрасов <gr421_mn@lab-407-4.tomtit-tomsk.ru>
2024-04-17 13:39:50 +0700

    first commit

---
+++ app/Controller/Site.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Controller;
+
+use Illuminate\Database\Capsule\Manager as DB;
+use Model\Post;
+use Src\View;
+use Src\Request;
+
+class Site
+{
+    public function index(Request $request): string
+    {
+        $posts = Post::where('id', $request->id)->get();
+        return (new View())->render('site.post', ['posts' => $posts]);
+    }
+
+
+    public function hello(): string
+    {
+        return new View('site.hello', ['message' => 'hello working']);
+    }
+}
---
+++ config/db.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'exfbiggp_m3',
+    'username' => 'exfbiggp',
+    'password' => 's1VrT6',
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => '',
+];
